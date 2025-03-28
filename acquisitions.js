dd6d1ab7a9c68987c8822ac0b09ff47482c75b70
NickAlvares <nickalvares@yandex.ru>
2025-03-28 18:45:25 +0500

    поправил js для вывода графика

--- acquisitions.js
+++ acquisitions.js
@@ -1,52 +1,46 @@
-import Chart from 'chart.js/auto'
+import Chart from 'chart.js/auto';
 
-(async function() {
-  const labels = ['Вовлечение уязвимых групп (20%)', 'Компоненты теории изменений (60%)', 'Обоснованность достижения соц. воздействия(20%)'];
+var acquisitions = document.getElementById('acquisitions');
+
+var labels = ['Вовлечение уязвимых групп (20%)', 'Компоненты теории изменений (60%)', 'Обоснованность достижения соц. воздействия(20%)'];
   
-  const data = {
-    labels: labels,
-    datasets: [
-      {
-        label: 'Автоматическая оценка',
-        data: [0.73, 0.811, 0.329],
-        backgroundColor: [red, orange, green]
-      }
-    ]
-  }
+var data = {
+  labels: labels,
+  datasets: [
+    {
+      label: 'Автоматическая оценка',
+      data: [0.73, 0.811, 0.329],
+      backgroundColor: [red, orange, green]
+    }
+  ]
+};
 
-  const config = {
-    type: 'polarArea',
-    data: data,
-    options: {
-      responsive: true,
-      scales: {
-        r: {
-          pointLabels: {
-            display: true,
-            centerPointLabels: true,
-            font: {
-              size: 18
-            }
-          }
-        }
-      },
-      plugins: {
-        legend: {
-          position: 'top',
-        },
-        title: {
-          display: true,
-          text: 'Визуализация социального аспекта'
+var chartOptions = {
+  responsive: true,
+  scales: {
+    r: {
+      pointLabels: {
+        display: true,
+        centerPointLabels: true,
+        font: {
+          size: 18
         }
       }
+    }
+  },
+  plugins: {
+    legend: {
+      position: 'top',
     },
+    title: {
+      display: true,
+      text: 'Визуализация социального аспекта'
+    }
   }
+}
 
-  new Chart(
-    document.getElementById('acquisitions'),
-    config
-  );
-
-  
-
-})();
+var polarChart = new Chart(acquisitions, {
+  type: 'polarArea',
+  data: data,
+  options: chartOptions
+});
