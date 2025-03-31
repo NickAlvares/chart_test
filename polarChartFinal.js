61cd85882349708141024020f308467b8b0dba48
NickAlvares <nickalvares@yandex.ru>
2025-03-31 15:17:45 +0500

    вывел радар на 1й план, поменял порядок вывода для остальных (сначала потенциалы, потом - оценки)

--- polarChartFinal.js
+++ polarChartFinal.js
@@ -10,15 +10,11 @@ var data = {
   labels: labelsAspects,
   datasets: [
     {
-      label: 'Автоматическая оценка',
-      data: [0.6, 0.31, 0.231],
-      backgroundColor: [
-        'rgba(255, 99, 133, 0.5)',
-        'rgba(54, 163, 235, 0.5)',
-        'rgba(255, 207, 86, 0.5)'
-      ]
+        type: 'radar',
+        labels: labelsSocialAspect,
+        data: [0.6, 0.78, 0.9]
     },
-    {
+        {
       label: 'Потенциал Автоматической оценки',
       data: [1, 0.5, 0.6],
       backgroundColor: [
@@ -28,6 +24,24 @@ var data = {
       ]
     },
     {
+        label: 'Потенциал оценки с верификацией эксперта',
+        data: [0, 1, 1],
+        backgroundColor: [
+          'rgba(255, 174, 191, 0.75)',
+          'rgba(113, 190, 241, 0.75)',
+          'rgba(250, 228, 174, 0.75)'
+        ]
+    },
+    {
+        label: 'Автоматическая оценка',
+        data: [0.6, 0.31, 0.231],
+        backgroundColor: [
+          'rgba(255, 99, 133, 0.5)',
+          'rgba(54, 163, 235, 0.5)',
+          'rgba(255, 207, 86, 0.5)'
+        ]
+    },
+    {
       label: 'Совокупная оценка',
       data: [0.6, 0.876, 0.739],
       backgroundColor: [
@@ -35,20 +49,6 @@ var data = {
         'rgba(0, 153, 255, 0.5)',
         'rgba(255, 183, 0, 0.5)'
       ]
-    },
-    {
-      label: 'Потенциал оценки с верификацией эксперта',
-      data: [0, 1, 1],
-      backgroundColor: [
-        'rgba(255, 174, 191, 0.75)',
-        'rgba(113, 190, 241, 0.75)',
-        'rgba(250, 228, 174, 0.75)'
-      ]
-    },
-    {
-        type: 'radar',
-        labels: labelsSocialAspect,
-        data: [0.6, 0.78, 0.9]
     }
   ]
 };
